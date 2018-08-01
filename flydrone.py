ffd094ebab76a8c737973fa00f7b0e9d9c2a0966
Kevin Meng <kevin.meng.01@gmail.com>
2018-08-01 17:31:34 -0500

    modify

--- flydrone.py
+++ flydrone.py
@@ -90,10 +90,10 @@ def on_press(key):
     except:
         if key == Key.up:
             print("thrust up")
-            set_velocity_body(vehicle, 0, 0, 0, 200)
+            set_velocity_body(vehicle, 0, 0, 0, 10)
         elif key == Key.down:
             print("thrust down")
-            set_velocity_body(vehicle, 0, 0, 0, -200)
+            set_velocity_body(vehicle, 0, 0, 0, -10)
         return
     if key.char == 'w':
         print("pitch forward")
