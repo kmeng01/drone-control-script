2e9e3414b0fb8ce7b5e6b70b5fb949f8453bd579
Kevin Meng <kevin.meng.01@gmail.com>
2018-08-01 22:58:28 -0500

    update test to support mouse scrolling

--- test_keylisten.py
+++ test_keylisten.py
@@ -1,10 +1,23 @@
 from pynput.keyboard import Key, Listener
+from pynput.mouse import Listener as Listener2
+
+# Mouse wheel
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    if not pressed:
+        # Stop listener
+        return False
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0}'.format(
+        (dx, dy)))
+
+# Keyboard
 def on_press(key):
     print('{0} pressed'.format(
         key))
-    print(key)
-    if key == Key.down:
-        print("im here")
 
 def on_release(key):
     print('{0} release'.format(
@@ -12,7 +25,7 @@ def on_release(key):
     if key == Key.esc:
         # Stop listener
         return False
-with Listener(
-    on_press=on_press,
-    on_release=on_release) as listener:
-    listener.join()
+
+with Listener(on_press=on_press,on_release=on_release) as listener:
+    with Listener2(on_scroll=on_scroll, on_click=on_click) as listener:
+        listener.join()
