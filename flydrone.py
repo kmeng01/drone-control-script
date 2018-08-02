eebafa10e1d77593eecbce0536d0bd45bff59771
Kevin Meng <kevin.meng.01@gmail.com>
2018-08-01 23:32:19 -0500

    add support for mouse wheel control and improved controls

--- flydrone.py
+++ flydrone.py
@@ -8,24 +8,19 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative, Command, Loca
 from pymavlink import mavutil
 
 from pynput.keyboard import Key, Listener
+from pynput.mouse import Listener as Listener2, Button
 import math
-
-#- Importing Tkinter: sudo apt-get install python-tk
-import Tkinter as tk
-
 
 #-- Connect to the vehicle
 print('Connecting...')
 vehicle = connect('udpout:192.168.42.1:14550')
+# vehicle = 0
 
 #-- Setup the commanded flying speed
-roll = 0
-yaw = 0
-pitch = 0
 thrust = 0
 
 #-- Define arm and takeoff
-def arm_and_takeoff(altitude):
+def init_motors():
 
    print("Arming motors")
    vehicle.mode = VehicleMode("GUIDED_NOGPS")
@@ -34,93 +29,104 @@ def arm_and_takeoff(altitude):
    while not vehicle.armed: time.sleep(1)
 
    print("Fly now!")
-   #vehicle.simple_takeoff(altitude)
-
-   #while True:
-   #   v_alt = vehicle.location.global_relative_frame.alt
-   #   print(">> Altitude = %.1f m"%v_alt)
-   #   if v_alt >= altitude - 1.0:
-   #       print("Target altitude reached")
-   #       break
-   #   time.sleep(1)
       
  #-- Define the function for sending mavlink velocity command in body frame
-def set_velocity_body(vehicle, d_roll, d_pitch, d_yaw, d_thrust):
-    
-    global roll
-    global pitch
-    global yaw
+def set_thrust(vehicle, d_thrust):
     global thrust
 
-    roll = roll + d_roll
-    pitch = pitch + d_pitch
-    yaw = yaw + d_yaw
-    thrust = thrust + d_thrust
-
-    print(roll, pitch, yaw, thrust)
-
-    eulerXYZ = [roll,pitch,yaw]
-
-    c1 = math.cos(eulerXYZ[0] / 2)
-    c2 = math.cos(eulerXYZ[1] / 2)
-    c3 = math.cos(eulerXYZ[2] / 2)
-    s1 = math.sin(eulerXYZ[0] / 2)
-    s2 = math.sin(eulerXYZ[1] / 2)
-    s3 = math.sin(eulerXYZ[2] / 2)
-    x = s1 * c2 * c3 + c1 * s2 * s3
-    y = c1 * s2 * c3 - s1 * c2 * s3
-    z = c1 * c2 * s3 + s1 * s2 * c3
-    w = c1 * c2 * c3 - s1 * s2 * s3
+    thrust = min(max(0,thrust + d_thrust),10)
+    print(thrust)
 
     msg = vehicle.message_factory.set_attitude_target_encode(
         0, 0, 0,
-        0b00000000,
-        (w,x,y,z),
-        1,1,1,
+        0b11111101,
+        (1,0,0,0),
+        0,0,0,
         thrust
     )
 
-    for _ in range(100): 
+    for _ in range(3): 
         vehicle.send_mavlink(msg)
         vehicle.flush()
 
+def set_euler(vehicle, roll, pitch, yaw, reset):
+    global thrust
+    print(roll, pitch, yaw, thrust)
+
+    x = roll
+    y = pitch
+    z = yaw
+    w = 1
+
+    msg = vehicle.message_factory.set_attitude_target_encode(
+        0, 0, 0,
+        0b00000010,
+        (w,x,y,z),
+        5,5,5,
+        thrust
+    )
+
+    epochs = 5
+    if reset:
+        epochs = 20
+
+    for _ in range(epochs): 
+        vehicle.send_mavlink(msg)
+        vehicle.flush()
+
+# Mouse wheel
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    if not pressed and button == Button.right:
+        return False
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0}'.format(
+        (dx, dy)))
+    if dy < 0:
+        print("thrust down")
+        set_thrust(vehicle, -1)
+    elif dy > 0:
+        print("thrust up")
+        set_thrust(vehicle, 1)
+
+# Keyboard
 def on_press(key):
     try:
 	    dummy = key.char
     except:
-        if key == Key.up:
-            print("thrust up")
-            set_velocity_body(vehicle, 0, 0, 0, 10)
-        elif key == Key.down:
-            print("thrust down")
-            set_velocity_body(vehicle, 0, 0, 0, -10)
         return
     if key.char == 'w':
         print("pitch forward")
-        set_velocity_body(vehicle, 0, -1, 0, 0)
+        set_euler(vehicle, 0, -10, 0, False)
     elif key.char == 's':
         print("pitch down")
-        set_velocity_body(vehicle, 0 , 1, 0, 0)
+        set_euler(vehicle, 0 , 10, 0, False)
     elif key.char == 'a':
         print("roll left")
-        set_velocity_body(vehicle, -1, 0, 0, 0)
+        set_euler(vehicle, -10, 0, 0, False)
     elif key.char == 'd':
         print("roll right")
-        set_velocity_body(vehicle, 1, 0, 0, 0)
+        set_euler(vehicle, 10, 0, 0, False)
 
 def on_release(key):
-    print('{0} release'.format(
-        key))
     if key == Key.esc:
-        # Stop listener
-        return False    
+        return False
+    try:
+	    dummy = key.char
+    except:
+        return
+    if key.char == 'w' or key.char == 'a' or key.char == 's' or key.char == 'd':
+        print("reset euler angles")
+        set_euler(vehicle, 0, 0, 0, True)
 
 #---- MAIN FUNCTION
 #- Takeoff
-arm_and_takeoff(10)
+init_motors()
 
 # Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+with Listener(on_press=on_press,on_release=on_release) as listener:
+    with Listener2(on_scroll=on_scroll, on_click=on_click) as listener:
+        listener.join()
